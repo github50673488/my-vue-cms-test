2c29d18dd280c1579e9c8d7f04ed67f6585e2498
liuguojun <gj.liu@sj-sol.com>
2021-05-05 12:23:28 +0900

    first app ,only data

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      msg: '123dd'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
