74f2882f596f6f4316d84675c8762d39e2b5ac2d
liuguojun <gj.liu@sj-sol.com>
2021-05-06 17:22:15 +0900

    11项目-完成首页中九宫格改造工程.pcf  改造 九宫格 区域的样式

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -5,7 +5,34 @@
         <img :src="lunbotuItem.img" alt="">
       </mt-swipe-item>
     </mt-swipe>
-    <h3>HomeContainer</h3>
+
+    <ul class="mui-table-view mui-grid-view mui-grid-9">
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+        <img src="../../assets/images/menu1.png" alt="">
+        <div class="mui-media-body">新闻资讯</div>
+      </a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+        <img src="../../assets/images/menu2.png" alt="">
+        <div class="mui-media-body">图片分享</div>
+      </a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+        <img src="../../assets/images/menu3.png" alt="">
+        <div class="mui-media-body">商品购买</div>
+      </a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+        <img src="../../assets/images/menu4.png" alt="">
+        <div class="mui-media-body">留言反馈</div>
+      </a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+        <img src="../../assets/images/menu5.png" alt="">
+        <div class="mui-media-body">视频专区</div>
+      </a></li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+        <img src="../../assets/images/menu6.png" alt="">
+        <div class="mui-media-body">联系我们</div>
+      </a></li>
+
+    </ul>
 
   </div>
 </template>
@@ -65,5 +92,23 @@ export default {
       height: 100%;
     }
   }
+}
+
+.mui-grid-view.mui-grid-9 {
+  background-color: #fff;
+  border: none;
+
+  img {
+    width: 60px;
+    height: 60px;
+  }
+
+  .mui-media-body {
+    font-size: 13px;
+  }
+}
+
+.mui-grid-view.mui-grid-9 .mui-table-view-cell {
+  border: none;
 }
 </style>
