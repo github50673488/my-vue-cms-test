2d1f0b8ac78bc627751b34cb25f238d2a66bb924
liuguojun <gj.liu@sj-sol.com>
2021-05-06 16:19:20 +0900

    09项目-完成首页轮播图样式布局.pcf   ,SCSS 的 & 操作符

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
+    <mt-swipe :auto="4000">
+      <mt-swipe-item>1</mt-swipe-item>
+      <mt-swipe-item>2</mt-swipe-item>
+      <mt-swipe-item>3</mt-swipe-item>
+    </mt-swipe>
     <h3>HomeContainer</h3>
+
   </div>
 </template>
 
@@ -12,4 +18,20 @@ export default {
 
 <style lang="scss" scoped>
 
+.mint-swipe {
+  height: 200px;
+
+  .mint-swipe-item {
+    &:nth-child(1) {
+      background-color: #e81616;
+    }
+    &:nth-child(2) {
+      background-color: #b89494;
+    }
+    &:nth-child(3) {
+      background-color: #35328e;
+    }
+
+  }
+}
 </style>
